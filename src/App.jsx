740a1eb28c7f558d8a9ded117077cf6db2d4c9d5
Gourav <161689015+gourav4222f@users.noreply.github.com>
2025-01-17 11:18:41 +0000

    uploding site with 3 section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Features from "./Sections/Features"
+import Header from "./Sections/Header"
+import Hero from "./Sections/Hero"
+import Pricing from "./Sections/Pricing"
+
+function App() {
+  return (
+    <main className=" overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Sections/Header.jsx
@@ -0,0 +1,79 @@
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react'
+import { Link as LinkScroll } from 'react-scroll'
+
+
+
+
+
+function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [hasScrolled, setHasScrolled] = useState(false);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScrolled(window.scrollY > 32);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [hasScrolled]);
+
+
+
+    const NavLink = ({ title }) => {
+        return (
+            <LinkScroll onClick={()=>setIsOpen(false)} to={title} offset={-100} spy smooth activeClass='nev-active' className=' base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5'>
+                {title}
+            </LinkScroll>
+        )
+    }
+
+
+    return (
+        <header className={clsx(' fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:p-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
+            <div className=' container flex h-14 items-center max-lg:px-5'>
+                <a href="" className=' lg:hidden flex-1 cursor-pointer z-2'>
+                    <img src='/public/images/xora.svg' alt='logo' width={115} height={55} className='' />
+                </a>
+
+                <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', isOpen ? ' max-lg:opacity-100' : ' max-lg:pointer-events-none')}>
+                    <div className=' max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
+                        <nav className=' max-lg:relative max-lg:z-2 max-lg:my-auto'>
+                            <ul className=' flex max-lg:block max-lg:px-12'>
+                                <li className=' nav-li'>
+                                    <NavLink title='features' />
+                                    <div className='dot'></div>
+                                    <NavLink title='pricing' />
+                                </li>
+                                <li className=' nav-logo'>
+                                    <LinkScroll className={clsx('max-lg:hidden transition-transform duration-500 cursor-pointer')} to='hero' offset={-300} spy={true} smooth={true} >
+                                        <img src='/public/images/xora.svg' alt='logo' width={160} height={55} />
+                                    </LinkScroll>
+                                </li>
+                                <li className='nav-li'>
+                                    <NavLink title='faq' />
+                                    <div className='dot'></div>
+                                    <NavLink title='download' />
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className=' lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90 '>
+                            <img src="/public/images/bg-outlines.svg" width={960} height={380} alt="" className=' relative z-2'/>
+                            <img src="/public/images/bg-outlines-fill.png" width={960} height={380} alt="" className=' absolute inset-0 mix-blend-soft-light opacity-5'/>
+                        </div>
+                    </div>
+                </div>
+
+                <button className=' lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center' onClick={() => setIsOpen(!isOpen)}>
+                    <img src={`/public/images/${isOpen ? "close" : "magic"}.svg`} alt='menu' width={24} height={24} />
+                </button>
+
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
